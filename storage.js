0d0f4dd0ccb217f16f79e84563c388bcc8e8dc3b
Felisias <159609165+Felisias@users.noreply.github.com>
2026-02-01 22:51:56 +0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,8 @@
-
+const Storage = {
+  load() {
+    return JSON.parse(localStorage.getItem("events") || "[]");
+  },
+  save(data) {
+    localStorage.setItem("events", JSON.stringify(data));
+  }
+};
