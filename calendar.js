0d2b802d89f8a83e3e7f06e3af71bf7fcbb6dd67
Felisias <159609165+Felisias@users.noreply.github.com>
2026-02-01 22:52:11 +0400

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -1,1 +1,13 @@
+const STEP = 15;
 
+function minutesToPx(m) {
+  return m * (60/60);
+}
+
+function snap(m) {
+  return Math.round(m/STEP)*STEP;
+}
+
+function overlaps(a,b) {
+  return a.start < b.end && b.start < a.end;
+}
