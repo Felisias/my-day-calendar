532f72fc9a33f6fdc844add28d7e7a8c1fdbbfe2
Felisias <159609165+Felisias@users.noreply.github.com>
2026-02-01 23:05:13 +0400

    Delete storage.js

--- storage.js
+++
@@ -1,8 +0,0 @@
-const Storage = {
-  load() {
-    return JSON.parse(localStorage.getItem("events") || "[]");
-  },
-  save(data) {
-    localStorage.setItem("events", JSON.stringify(data));
-  }
-};
