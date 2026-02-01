4283e09c149cd303a31d8ce7d74e328aa05ea012
Felisias <159609165+Felisias@users.noreply.github.com>
2026-02-01 22:13:42 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open("cal").then(c=>c.addAll([
+      "./",
+      "./index.html",
+      "./style.css",
+      "./app.js"
+    ]))
+  );
+});
