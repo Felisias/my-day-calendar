c59535365eda682b39436dfd09a9c4d0925c2209
Felisias <159609165+Felisias@users.noreply.github.com>
2026-02-02 00:07:41 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,54 +1,8 @@
-const CACHE_NAME = 'personal-calendar-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './manifest.json'
-];
-
-// Установка Service Worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+// Простой Service Worker для кэширования
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
 });
 
-// Активация и очистка старых кэшей
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Стратегия: сначала сеть, потом кэш
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        // Клонируем ответ для кэширования
-        const responseToCache = response.clone();
-        
-        caches.open(CACHE_NAME)
-          .then(cache => {
-            cache.put(event.request, responseToCache);
-          });
-          
-        return response;
-      })
-      .catch(() => {
-        return caches.match(event.request);
-      })
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
 });
