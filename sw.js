2725505d3d0b2d558447fc5158ddb4d58dd825cd
Felisias <159609165+Felisias@users.noreply.github.com>
2026-02-01 22:40:15 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("planner").then(c=>c.addAll([
-      "./","./index.html","./style.css","./app.js"
-    ]))
-  );
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open("p").then(c=>c.addAll([
+  "./","./index.html","./style.css","./app.js"
+ ])))
 });
